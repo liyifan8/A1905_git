851a3fb13a7d031f5a04df026bd6d4bd04fe7705
liyifan <1952452952@qq.com>
2021-11-10 09:15:25 +0800

    测试

--- demo/src/main/java/com/example/demo/User.java
+++ demo/src/main/java/com/example/demo/User.java
@@ -17,7 +17,10 @@ public class User {
     private List<String> idCards;
     private List<Address> addressList;
     private Address address;
-
+//测试冲突
+public void User(){
+return;
+}
     @Override
     public String toString() {
         return "User{" +
@@ -31,67 +34,67 @@ public class User {
                 ", address=" + address +
                 '}';
     }
-
+    
     public String getUsername() {
         return username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
     public Integer getAge() {
         return age;
     }
-
+    
     public void setAge(Integer age) {
         this.age = age;
     }
-
+    
     public Boolean getSex() {
         return sex;
     }
-
+    
     public void setSex(Boolean sex) {
         this.sex = sex;
     }
-
+    
     public Date getBorn() {
         return born;
     }
-
+    
     public void setBorn(Date born) {
         this.born = born;
     }
-
+    
     public Map<String, String> getParent() {
         return parent;
     }
-
+    
     public void setParent(Map<String, String> parent) {
         this.parent = parent;
     }
-
+    
     public List<String> getIdCards() {
         return idCards;
     }
-
+    
     public void setIdCards(List<String> idCards) {
         this.idCards = idCards;
     }
-
+    
     public List<Address> getAddressList() {
         return addressList;
     }
-
+    
     public void setAddressList(List<Address> addressList) {
         this.addressList = addressList;
     }
-
+    
     public Address getAddress() {
         return address;
     }
-
+    
     public void setAddress(Address address) {
         this.address = address;
     }
